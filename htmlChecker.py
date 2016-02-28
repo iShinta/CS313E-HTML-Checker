b232309e51bb2178e8c2b08bf52c849565ce7a5e
iShinta <ho_minh_tri@yahoo.fr>
2016-02-28 10:12:10 -0600

    Added 3 main files
    htmlChecker.py is the main program
    htmlfile.txt is the test sample
    instructions.txt details the objectives of the program

---
+++ htmlChecker.py
@@ -0,0 +1,17 @@
+#  File: htmlChecker.py
+#  Description: Verifies in HTML file if tags are correctly matched
+#  Student's Name: Minh-Tri Ho
+#  Student's UT EID: mh47723
+#  Course Name: CS 313E
+#  Unique Number: 50940
+#
+#  Date Created: 02/28/16
+#  Date Last Modified: 02/28/16
+
+def main()
+    file_name = "htmlfile.txt"
+
+    html_file = open('./' +file_name, 'r')
+    
+
+main()
