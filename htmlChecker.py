c971a439a68103f6dfcb0cacf55252d319827f5b
iShinta <ho_minh_tri@yahoo.fr>
2016-02-28 10:56:57 -0600

    v0.1
    First Stack of raw tags

--- htmlChecker.py
+++ htmlChecker.py
@@ -8,10 +8,39 @@
 #  Date Created: 02/28/16
 #  Date Last Modified: 02/28/16
 
-def main()
-    file_name = "htmlfile.txt"
+import stack
 
+def main():
+    file_name = "htmlfile.txt"
     html_file = open('./' +file_name, 'r')
-    
+
+    #Where the tags are stored in order
+    tags = stack.Stack()
+    tags_marker = "<>"
+
+    #Initialization of checker variables
+    tag_open = False
+
+    for line in html_file:
+
+        #Extracts the tags
+        for i in range(len(line)):
+            if(line[i] == tags_marker[0]):
+                if(tag_open == True):
+                    print("[Error]: Tag already opened")
+                else:
+                    tag_open = True
+                    tag = ""
+            elif(line[i] == tags_marker[1]):
+                if(tag_open == False):
+                    print("[Error]: Closing Tag without opening one")
+                else:
+                    tag_open = False
+                    tags.push(tag)
+                    print("Added tag:",tag)
+            else:
+                if(tag_open == True):
+                    tag += (line[i])
+
 
 main()
